027678e1e2c9e23b96f8a144c6801269589d40b4
iamstyx <vtechnical1804@gmail.com>
2024-03-17 16:32:20 +0530

    TODO: Backend Done

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://vsushant8421:xgHf2jFAXr51Jy9a@cluster0.nvfbpn8.mongodb.net/")
+mongoose.connect("mongodb+srv://vsushant8421:xgHf2jFAXr51Jy9a@cluster0.nvfbpn8.mongodb.net/todos")
 
 const todoSchema = mongoose.Schema({
     title: String,
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,9 @@ const {createTodo, updateTodo} = require("./types");
 const app = express();
 const port = 3000;
 
-
-
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
 
     const createPayload = req.body;
     const parsePaylod = createTodo.safeParse(createPayload);
@@ -18,13 +16,28 @@ app.post("/todo", (req, res) => {
       return;
     }
     // put it in mongodb
+    await todo.create({
+      title: createPayload.title,
+      description: createPayload.description,
+      completed :false
+    })
+
+    res.json({
+      msg:"Todo Created"
+    })
 })
 
-app.get("/todos", (req, res) => {
+app.get("/todos", async (req, res) => {
+
+  const todos = await todo.find({});
+  res.json({
+    todos
+  })
+
 
 })
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async(req, res) => {
 
     const updatePayload = req.body;
     const parsePaylod = updateTodo.safeParse(updatePayload)
@@ -34,6 +47,14 @@ app.put("/completed", (req, res) => {
       })
       return;
     }
+    await todo.update({
+      _id:req.body._id
+    },{
+      completed:true
+    })
+    res.json({
+      msg: "Todo marked as completed"
+    })
 })
 
 
